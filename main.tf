dc7d31da43bef387ac695800fe49b7cf5a0a5e81
YeshwanthRamesh <43970137+YeshwanthRamesh@users.noreply.github.com>
2024-04-21 00:19:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_linux_virtual_machine" "TerraformMachine" {
   disable_password_authentication = false
   admin_ssh_key {
     username   = "TerraformMachine"
-    public_key = file(".")  
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCoc6GnTCmyRe41OlXq4FYQABqnEa7U7K6nzPytadgQObo708kvaW9IbnhOoDIacPcDo+Kj/DmKwl59+pjptKbo38bKP7A/e9VTcfIlJQDGgKpXp41Z2riaUrIEjPwgLdM6CnWObWlqPTtMduxSkrTel1lqrG35n60YgeNEiIjJNUBdtR2rbTsMGkNGlgozIQj7zPQqs/5SA9gT6/ElV2F7DEtyEpWwQb4G5ZgftwwT7WtuUAkX6L9EmyWg3Q1vt9JjhMmduBY6FgvQOc8ItZjgUsF3iM5vwlmdIWrEf6Bg126j8XYu75Nf8nSR4Et8vWdxi9hMMFYuC9TI7B3ixsEA7QWKn2GayVqO6yI6msyWmCPpkISbdXs808HS/ts5FoyJrDApcXfh1mYeWtTO2YkivWPxo625bsS/pxeJDqF67PHuOl8ruYvPz/rdxaeUk7mJZPcmN/bxVAWmlC8xrlXwuknpKWYahMHgmE4liLp2WIyI3xudb0tG5W7t9oqpk3E= generated-by-azure"  
   }
 
   network_interface_ids = [azurerm_network_interface.YeshwanthNetworkInterface.id]
